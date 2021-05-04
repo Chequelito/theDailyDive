8c9458d2f3eea518c87d9e0df0ec918d43d4af7a
Zach Raab <zpraab@hotmail.com>
2021-05-04 15:26:30 -0600

    pseudo code and api key/url declarations, user geolocate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,52 @@
 // The Daily Dive
 
+console.log("js linked");
+
 // feature/js-creation  //
 //      author: Zach    //
+//  Pseudo Code  //
 
 // VARIABLE DECLARATIONS
 
+var newsBaseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+var weatherBaseURL = `api.openweathermap.org/data/2.5/weather?`;
+var newsAPIKey = "B1bWnHrsG4FxF0rkw1Fg9cWo0bLCYrtE";
+var weatherAPIKey = "24d3e77575ea6a3daa1e23b95dbe112f";
+
 // FUNCTION DECLARATIONS
 
-// EVENT LISTENERS
+// link html elements to js variables
 
-// Pseudo Code
+// pull user's locations from browser with window.navigator.geolocation
 
-// link html to js file
+function geolocateUser() {
+  if (window.navigator && window.navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      onGeolocateSuccess,
+      onGeolocateError
+    );
+  }
+  console.log("geolocate");
+  var geolactionObj = {
+    lat: 0,
+    lon: 0,
+  };
+  callWeatherAPI(geolactionObj);
+  callNewsAPI(geolactionObj);
+}
+
 // call API'S
 //      Weather
+function callWeatherAPI() {
+  console.log("weather api call function");
+  var weatherURL = `${weatherBaseURL}lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`;
+}
 //      News
+function callNewsAPI() {
+  console.log("news api call function");
+  var newsURL = `${newsBaseURL}fq=news_desk:"technology"&glocations:${____}&api-key=${newsAPIKey}`;
+}
 
-console.log("js linked");
+// on page load, display:
+//      weather based on location
+//      tech news articles
